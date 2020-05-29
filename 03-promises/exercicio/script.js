6f52012dc18d4cb3348166b42b2191e0a15ae5f8
deboranis <dc.nisen@hotmail.com>
2020-05-29 15:07:42 -0300

    solucoes 1

--- 03-promises/exercicio/script.js
+++ 03-promises/exercicio/script.js
@@ -1,15 +1,15 @@
 const promise = new Promise((resolve, reject) => {
-  if (condicao) {
-    resolve("resolvido!"); // dado é retornado para o then
-  } else {
-    reject("aaahh errooou"); // entra no catch
-  }
+    if (condicao) {
+        resolve("resolvido!"); // dado é retornado para o then
+    } else {
+        reject("aaahh errooou"); // entra no catch
+    }
 });
 
 promise
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
