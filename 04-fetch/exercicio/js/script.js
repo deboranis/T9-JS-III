f60754a4a72138fe7d52cf51dc5ab650902856d2
deboranis <dc.nisen@hotmail.com>
2020-05-30 14:57:26 -0300

    solucoes sabado

--- 04-fetch/exercicio/js/script.js
+++ 04-fetch/exercicio/js/script.js
@@ -1,7 +1,8 @@
 const cardSection = document.getElementById('cards-section')
 const errorSection = document.getElementById('error')
 const loading = document.getElementById('spinner')
-// Caso os cards não carreguem, mostraremos isso:
+const chamarNovaCarta = document.getElementById('chamarNovaCarta')
+    // Caso os cards não carreguem, mostraremos isso:
 const placeholderCard = {
     nome: "Carta não carregada",
     tipo: "Arcano Maior",
@@ -12,5 +13,18 @@ const placeholderCard = {
 
 renderizaCarta = carta => {
     const novaCarta = new Card(carta).render()
-    cardSection.innerHTML = novaCarta 
+    cardSection.innerHTML = novaCarta
 }
+
+//
+renderizaCarta(placeholderCard); // só pra aparecer o template vazio da carta
+
+chamarNovaCarta.addEventListener('click', () => {
+    fetch(`https://raw.githubusercontent.com/reprograma/T9-JS-III/master/04-fetch/exercicio/tarot.json`) // url pra qual será feita a requisição
+        .then((response) => { return response.json() }) // transforma o json em objeto javascript
+        .then((dados) => { // vamos processar os dados
+            const numAleatorio = Math.floor(Math.random() * dados.length); // aqui estamos sorteando um numero: floor serve pra arredondar pra baixo o numero do random, que por sua vez é feito com base no comprimento do "dados"
+
+            renderizaCarta(dados[numAleatorio]); // estamos passando pra funcao renderizaCarta um item da array dados
+        })
+})
